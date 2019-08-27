6f487c2264309bc69fc678ffc33250484f45c32a
Abss <33752861+Abss0x7tbh@users.noreply.github.com>
2019-08-27 21:08:40 +0530

    Tool now uses massdns as a defualt
    Thanks to Patrik Hudak on helping me make things simple.

--- massns.sh
+++ massns.sh
@@ -1,12 +1,13 @@
 #$1 - domain whose auth NS is to be found out
 #$2 - Target domain list. subdomains of the Target
 
-mkdir /root/massNS/
+#mkdir /root/massNS/
 mkdir /root/massNS/$1/
 touch ~/massNS/$1/$1_ns_ip.txt
 touch ~/massNS/$1/$1_ns.txt
 touch ~/massNS/$1/$1_op.txt
-touch ~/massNS/$1/$1_op_success.txt
+touch ~/massNS/$1/$1_op_cname.txt
+touch ~/massNS/$1/$1_ns_cidr.txt
 
 getNS () {
 host -t ns $1 | awk '{ print $(NF) }' | sed 's/\.$//' > ~/massNS/$1/$1_ns.txt
@@ -42,22 +43,11 @@ masscan -iL ~/massNS/$1/$1_ns_cidr.txt -p 53 --rate=1000 | awk '{print $(NF)}' >
 
 }
 
-resolvers () {
-
-while IFS= read -r line
-do
-	#select random input from nameservers file
-	random_ns=$(shuf -n 1 ~/massNS/$1/$1_resolvers.txt |xargs ) 
-	
-	echo $random_ns >> ~/massNS/$1/$1_resolvers_used.txt
-
-done<$2
-
-}
 
 resolve () {
 
-interlace -pL ~/massNS/$1/$1_resolvers_used.txt -tL $2 -o ~/massNS/$1/$1_op.txt -threads 5 -c "nslookup _target_  _proxy_ >> _output_" -v
+#interlace -pL ~/massNS/$1/$1_resolvers_used.txt -tL $2 -o ~/massNS/$1/$1_op.txt -threads 5 -c "nslookup _target_  _proxy_ >> _output_" -v
+massdns -r ~/massNS/$1/$1_resolvers.txt -o J -t A -q --flush $2 | jq -r '.'  > ~/massNS/$1/$1_op.txt
 
 }
 
@@ -82,27 +72,46 @@ echo "Active resolvers are :"
 echo "----------------------------"
 cat ~/massNS/$1/$1_resolvers.txt
 echo "-----------------------------"
-echo "Total Number of Resolvers : `wc -l ~/massNS/$1/$1_resolvers.txt`"
-echo "Randomly Picking resolvers to be used with the target list at $2"
-resolvers $1 $2
+#echo "Total Number of Resolvers : `wc -l ~/massNS/$1/$1_resolvers.txt`"
+#echo "Randomly Picking resolvers to be used with the target list at $2"
+#resolvers $1 $2
 echo "----"
-echo "Resolving target list using Interlace"
+echo "Resolving target list using massdns"
 echo "------------------------------"
 resolve $1 $2
-echo "All done. Output file at ~/massNS/$1/$1_op_success.txt"
+echo "All done! "
 echo "------------------------------"
-echo "Successfully resolved the following domains "
+echo "Results :  "
 echo "-------------------------------------"
-cat ~/massNS/$1/$1_op.txt | grep -E "^(Name|Address)" > ~/massNS/$1/$1_tmp.txt
-grep -v -f ~/massNS/$1/$1_resolvers_used.txt ~/massNS/$1/$1_tmp.txt > ~/massNS/$1/$1_op_success.txt
-column -x ~/massNS/$1/$1_op_success.txt
+#cat ~/massNS/$1/$1_op.txt | grep -E "^(Name|Address)" > ~/massNS/$1/$1_tmp.txt
+#grep -v -f ~/massNS/$1/$1_resolvers_used.txt ~/massNS/$1/$1_tmp.txt > ~/massNS/$1/$1_op_success.txt
+#column -x ~/massNS/$1/$1_op_success.txt
 #make a file for canonical name entires
-cat ~/massNS/$1/$1_op.txt | grep canonical > ~/massNS/$1/$1_op_cname.txt
+#cat ~/massNS/$1/$1_op.txt | grep canonical > ~/massNS/$1/$1_op_cname.txt
 #dig could also be used only for ips
 #only ips
-echo "Resolved list of IP addresses can be found at ~/massNS/$1/$1_op_success_ip.txt. Opening now "
+cat ~/massNS/$1/$1_op.txt | jq
+echo "---------------------------------"
+
+#only ip
+cat ~/massNS/$1/$1_op.txt | jq -r 'if .resp_type =="A" then .data else empty end' > ~/massNS/$1/$1_op_ip.txt
+cat ~/massNS/$1/$1_op.txt | jq -r 'if .resp_type =="CNAME" then .data else empty end' > ~/massNS/$1/$1_op_cname.txt
+echo "The resolved  IP addresses are : "
 echo "-----------------------------------"
-cat ~/massNS/$1/$1_op_success.txt | grep -E "Address" | awk '{print $2}' > ~/massNS/$1/$1_op_success_ip.txt
-cat ~/massNS/$1/$1_op_success_ip.txt
+#cat ~/massNS/$1/$1_op_success.txt | grep -E "Address" | awk '{print $2}' > ~/massNS/$1/$1_op_success_ip.txt
+cat ~/massNS/$1/$1_op_ip.txt
 echo "-------------------------------"
+echo "Stats"
+echo "-------------------------------"
+echo "[*] Resolvers obtained and used : `cat ~/massNS/$1/$1_resolvers.txt | wc -l`"
+echo "[*] Target List had `cat $2 | wc -l`  domains"
+echo "[*] A records obtained for `cat ~/massNS/$1/$1_op_ip.txt | wc -l`  domains"
+echo "[*] CNAME records obtained for `cat ~/massNS/$1/$1_op_cname.txt | wc -l` domains"
+echo "--------------------------------"
+echo "Output Files :"
+echo "--------------------------------"
+echo "[*] Resolved IP Addresses : ~/massNS/$1/$1_op_ip.txt"
+echo "[*] Generic JSON format output :  ~/massNS/$1/$1_op.txt"
+echo "[*] DNS Provider CIDR range that were probed :  ~/massNS/$1/$1_ns_cidr.txt"
+echo "---------------------------------"
 
